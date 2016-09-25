30afbc3fdd7c4c17f8ee450ae2161d846841c378
ice <ice@THANHTRAN>
2016-09-25 17:55:54 +0700

    Add project files.

---
+++ FoodExpress/Controllers/MethodController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FoodExpress.Controllers
+{
+    public class MethodController : Controller
+    {
+        // GET: Method
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
